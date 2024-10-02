e2eec9b630daf05b046425d79c99928bc89a0d0c
Mdumiseni <47352274+mdumiseni@users.noreply.github.com>
2024-10-02 21:42:34 +0200

    Update Marketstack.cs
    Added comment documentation to the library

--- Marketstack/Marketstack/Marketstack.cs
+++ Marketstack/Marketstack/Marketstack.cs
@@ -23,6 +23,23 @@ public class Marketstack(string accessKey, IHttpClientFactory httpClientFactory)
         }
     }
 
+    /// <summary>
+    ///  Intraday prices are available for all US stock tickers included in the IEX (Investors Exchange) stock exchange.
+    /// </summary>
+    /// <param name="symbol">The stock ticker symbol of the current data object.</param>
+    /// <param name="startDate">Filter results based on a specific timeframe by passing an end-date in YYYY-MM-DD format. You can also specify an exact time in ISO-8601 date format, e.g. 2020-05-21T00:00:00+0000.</param>
+    /// <param name="endDate">Filter results based on a specific timeframe by passing an end-date in YYYY-MM-DD format. You can also specify an exact time in ISO-8601 date format, e.g. 2020-05-21T00:00:00+0000.</param>
+    /// <returns></returns>
+    public async Task<EndOfDay?> GetHistoricalData(string symbol, DateTime startDate, DateTime endDate)
+    {
+        return await GetTickerEndOfDay(new EodParameters()
+        {
+            Symbols = symbol,
+            DateTo = endDate,
+            DateFrom = startDate
+        });
+    }
+
     private string GetEndOfDayParameterList(EodParameters? requestParams)
     {
         if (requestParams is null)
